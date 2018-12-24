66a060cf8663586672d5ee05506c18af4889f16f
lws803 <lws803@gmail.com>
2018-12-24 15:39:04 +0800

    Added try and except block for float conversion

--- src/uwb_package/scripts/uwb_driver.py
+++ src/uwb_package/scripts/uwb_driver.py
@@ -6,12 +6,33 @@ from time import sleep
 from generic_msgs.msg import UWB
 import rospy
 
-with serial.Serial('/dev/tty.usbmodem0007600080851', 115200, timeout=1) as ser:
-    counter = 1
-    while True:
+def serialReader(ser):
+    pub = rospy.Publisher('uwb_tlm', UWB, queue_size=1)
+
+    rospy.init_node('uwb_tlm_node', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+
+    while not rospy.is_shutdown():
         line = ser.readline()   # read a '\n' terminated line
-        print (str(counter) + ": " + line)
         result = [x.strip() for x in line.split(',')]
-        print (result)
-        counter += 1
-        sleep(.1)
+        rospy.loginfo(line)
+
+        if (len(result) >= 3):
+            message = UWB()
+            try:
+                message.a = float(result[0])
+                message.b = float(result[1])
+                message.c = float(result[2])
+
+                pub.publish(message)
+            except:
+                rospy.loginfo("Could not convert message to 3 floats")
+
+        rate.sleep()
+
+if __name__ == '__main__':
+    with serial.Serial('/dev/ttyACM0', 115200, timeout=1) as ser:
+        try:
+            serialReader(ser)
+        except rospy.ROSInterruptException:
+            pass
