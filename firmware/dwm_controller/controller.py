ee7bf624a2ef08bf928914fd9781841b65738dd8
lekoook <kokteng1313@gmail.com>
2019-01-09 04:56:02 +0800

    Improve controller application
    Improve the controller to be a more complete, with more features.
    
    Controller can do:
      1. Display distances
      2. Start device
      3. Stop device
      4. Switch device to Tag
      5. Switch device to Anchor
      6. Set the device address (ID)
      7. Send switch command to device
      8. Configure the variables for above commands

--- firmware/dwm_controller/controller.py
+++ firmware/dwm_controller/controller.py
@@ -1,14 +1,38 @@
 import serial
 import keyboard
 import sys, termios
+import re
 
 # Variables to be used
-devicePort = serial.Serial('/dev/ttyACM1', 115200, timeout=1)
+devicePort = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 needMenu = True
 selection = 0
 
+# Devic control configuration variables
+givenDeviceId = '0'
+givenAnchorsCount = '3'
+givenSwitchList = ["12a", "23a", "01a", "30t"]
+
+# Device control variables
+commandKey = ''
+newDeviceId = ''
+anchorsCount = ''
+switchesNum = ''
+switchList = []
+
+# Message strings
+MSG_INVALID = "Invalid selection"
+MSG_SELECT_NO = "Select no: "
+MSG_GIVE_VAL = "Give a new value: "
+MSG_GIVE_SWITCHES = "Give switches separated by ' ' space (eg. 12a 23a 31a): "
+MSG_INVALID_SWITCHES = "Invalid characters"
+MSG_CONFIG_TITLE = "*** CONFIG VALUES ***"
+MSG_SEPARATOR = "---------------------"
+MSG_RESULT = "--- RESULT ---"
+MSG_SERIAL_SENT = "Serial transmitted: "
+
 # Constants
-MAX_SELECTIONS = 7
+MAX_SELECTIONS = 8
 SELECTION_OUTPUT = 1
 SELECTION_START = 2
 SELECTION_STOP = 3
@@ -16,14 +40,63 @@ SELECTION_TAG = 4
 SELECTION_ANCHOR = 5
 SELECTION_ADDRESS = 6
 SELECTION_SWITCH = 7
+SELECTION_CONFIG = 8
+CHAR_START = 'b'
+CHAR_STOP = 's'
+CHAR_TAG = 't'
+CHAR_ANCHOR = 'a'
+CHAR_SWITCH = 'w'
+CHAR_ADDRESS = 'd'
+CHAR_TERM = ';'
+MAX_CONFIG_SELECTIONS = 4
+CONFIG_ID = 1
+CONFIG_ANCHORS = 2
+CONFIG_SWITCHES = 3
 
 # Data bytes to be sent over the port
 startData = b"b;"
 stopData = b"s;"
 tagData = b"t03;"
 anchorData = b"a13;"
-addressData = b"d0;"
-switchData = b"w00t;"
+addressData = b"d1;"
+switchData = b"w412a23a01a30t;"
+
+def serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList):
+    serialOutput = ""
+
+    serialOutput += commandKey
+    if commandKey == CHAR_START or commandKey == CHAR_STOP:
+        pass
+    elif commandKey == CHAR_TAG or commandKey == CHAR_ANCHOR:
+        serialOutput += newDeviceId
+        serialOutput += anchorsCount
+    elif commandKey == CHAR_ADDRESS:
+        serialOutput += newDeviceId
+    elif commandKey == CHAR_SWITCH:
+        serialOutput += switchesNum
+        for switch in switchList:
+            serialOutput += switch
+    else:
+        pass
+
+    serialOutput += CHAR_TERM
+    print(MSG_SERIAL_SENT + "\"" + serialOutput + "\"")
+    return serialOutput
+
+
+def clearControlVars():
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = ''
+    newDeviceId = ''
+    anchorsCount = ''
+    switchesNum =''
+    switchList = []
+
 
 def readDistance():
     line = devicePort.readline()
@@ -39,7 +112,16 @@ def readDistance():
     pass
 
 
+def printConfigStatus():
+    print(MSG_CONFIG_TITLE)
+    print("* ID            : " + givenDeviceId)
+    print("* Anchors Count : " + givenAnchorsCount)
+    print("* Switches      : " + str(givenSwitchList))
+
+
 def printMenu():
+    print("")
+    printConfigStatus()
     print("")
     print("%d. Display PORT output" % SELECTION_OUTPUT)
     print("%d. Send START command" % SELECTION_START)
@@ -48,48 +130,151 @@ def printMenu():
     print("%d. Send ANCHOR command" % SELECTION_ANCHOR)
     print("%d. Send ADDRESS command" % SELECTION_ADDRESS)
     print("%d. Send SWITCH command" % SELECTION_SWITCH)
+    print("%d. Configure switches" % SELECTION_CONFIG)
     pass
 
 
-def showMenu() -> int:
+def printConfigMenu():
+    print("")
+    print("Choose to configure: ")
+    print("%d. Device ID" % CONFIG_ID)
+    print("%d. Anchors Count" % CONFIG_ANCHORS)
+    print("%d. Switches" % CONFIG_SWITCHES)
+
+
+def showMenu():
     printMenu()
     termios.tcflush(sys.stdin, termios.TCIOFLUSH)
-    selection = int(input("Select no: "))
+    print(MSG_SEPARATOR)
+    selection = int(input(MSG_SELECT_NO))
     print("")
     return selection
 
 
 def sendStart():
-    devicePort.write(startData)
-    print("Started device.\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_START
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Started device.")
+    clearControlVars()
     pass
 
 def sendStop():
-    devicePort.write(stopData)
-    print("Stopped device.\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_STOP
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Stopped device.")
+    clearControlVars()
     pass
 
 def sendTag():
-    devicePort.write(tagData)
-    print("Switched device to Tag.\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_TAG
+    newDeviceId = givenDeviceId
+    anchorsCount = givenAnchorsCount
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Switched device to Tag.")
+    clearControlVars()
     pass
 
 def sendAnchor():
-    devicePort.write(anchorData)
-    print("Switched device to Anchor.\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_ANCHOR
+    newDeviceId = givenDeviceId
+    anchorsCount = givenAnchorsCount
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Switched device to Anchor.")
+    clearControlVars()
     pass
 
 
 def sendAddress():
-    devicePort.write(addressData)
-    print("Switched device to ID:\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_ADDRESS
+    newDeviceId = givenDeviceId
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Switched device to ID: %d" % int(newDeviceId))
+    clearControlVars()
     pass
 
 
 def sendSwitch():
-    devicePort.write(switchData)
-    print("Executed switches:\n")
+    global commandKey
+    global newDeviceId
+    global anchorsCount
+    global switchesNum
+    global switchList
+
+    commandKey = CHAR_SWITCH
+    switchList = givenSwitchList
+    switchesNum = str(len(switchList))
+    devicePort.write(bytearray(serialDataBuilder(commandKey, newDeviceId, anchorsCount, switchesNum, switchList), 'utf-8'))
+    print("Executed switches: " + str(switchList))
+    clearControlVars()
     pass
+
+
+def configSwitches(raw):
+    global givenSwitchList
+
+    doesMatch = re.match(r'\d\d[a - z](\s\d\d[a - z])*', raw, 0)
+    if not doesMatch:
+        print(MSG_INVALID_SWITCHES)
+        return
+
+    givenSwitchList = re.split('\s', raw) # Split any whitespace
+    givenSwitchList = list(filter(None, givenSwitchList)) # Remove all the empty elements
+
+
+def configure():
+    global givenDeviceId
+    global givenAnchorsCount
+    global givenSwitchList
+
+    printConfigMenu()
+    termios.tcflush(sys.stdin, termios.TCIOFLUSH)
+    print(MSG_SEPARATOR)
+    selection = int(input(MSG_SELECT_NO))
+    print("")
+
+    if selection == CONFIG_ID:
+        value = input(MSG_GIVE_VAL)
+        print("")
+        givenDeviceId = value
+    elif selection == CONFIG_ANCHORS:
+        value = input(MSG_GIVE_VAL)
+        print("")
+        givenAnchorsCount = value
+    elif selection == CONFIG_SWITCHES:
+        raw = input(MSG_GIVE_SWITCHES)
+        configSwitches(raw)
+    else:
+        print(MSG_INVALID)
 
 
 def parseSelection():
@@ -97,19 +282,27 @@ def parseSelection():
         global needMenu
         needMenu = False
     elif selection == SELECTION_START:
+        print(MSG_RESULT)
         sendStart()
     elif selection == SELECTION_STOP:
+        print(MSG_RESULT)
         sendStop()
     elif selection == SELECTION_TAG:
+        print(MSG_RESULT)
         sendTag()
     elif selection == SELECTION_ANCHOR:
+        print(MSG_RESULT)
         sendAnchor()
     elif selection == SELECTION_ADDRESS:
+        print(MSG_RESULT)
         sendAddress()
     elif selection == SELECTION_SWITCH:
+        print(MSG_RESULT)
         sendSwitch()
+    elif selection == SELECTION_CONFIG:
+        configure()
     else:
-        print("Invalid selection.\n")
+        print(MSG_INVALID)
 
 
 def switchOnMenu(e):
@@ -126,9 +319,6 @@ keyboard.hook(switchOnMenu)
 while (True):
     if needMenu:
         selection = showMenu()
-        if selection > MAX_SELECTIONS or selection < 0:
-            print("Invalid selection.\n")
-            pass
 
         parseSelection()
     else:
