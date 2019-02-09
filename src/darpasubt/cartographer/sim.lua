62785aa55908a91b72f612c99dfcc8f2d67a9be6
LeeYiyuan <leeyiyuan@hotmail.com>
2019-02-09 16:07:08 +0800

    Performance tweaks

--- src/darpasubt/cartographer/sim.lua
+++ src/darpasubt/cartographer/sim.lua
@@ -43,11 +43,14 @@ options = {
 }
 
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+TRAJECTORY_BUILDER_3D.motion_filter.max_time_seconds = 1
+TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_3D.motion_filter.max_angle_radians = 0.05
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 4
 POSE_GRAPH.optimization_problem.huber_scale = 5e2
-POSE_GRAPH.optimize_every_n_nodes = 320
+POSE_GRAPH.optimize_every_n_nodes = 0
 POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
 POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 10
 POSE_GRAPH.constraint_builder.min_score = 0.62
--- src/darpasubt/src/mapping_cloud_publisher.cpp
+++ src/darpasubt/src/mapping_cloud_publisher.cpp
@@ -18,10 +18,7 @@ private:
   ros::Publisher cloud_pub_;
   ros::Timer timer_;
 
-  std::vector<int> submap_indices_;
-
   void timerCallback(const ros::TimerEvent& timer);
-  void submapListCallback(const cartographer_ros_msgs::SubmapList::ConstPtr submap_list);
 };
 
 MappingCloudPublisher::MappingCloudPublisher()
@@ -29,49 +26,29 @@ MappingCloudPublisher::MappingCloudPublisher()
   ros::NodeHandle handle;
   ros::NodeHandle handle_private("~");
 
-  submap_list_sub_ = handle.subscribe("/submap_list", 1, &MappingCloudPublisher::submapListCallback, this);
   client_ = handle.serviceClient<cartographer_ros_msgs::SubmapCloudQuery>("submap_cloud_query");
   cloud_pub_ = handle.advertise<sensor_msgs::PointCloud2>("/X1_1/mapping_cloud", 1);
-  timer_ = handle.createTimer(ros::Duration(5), &MappingCloudPublisher::timerCallback, this);
+  timer_ = handle.createTimer(ros::Duration(1), &MappingCloudPublisher::timerCallback, this);
 }  
 
 void MappingCloudPublisher::timerCallback(const ros::TimerEvent& timer)
 {
-  pcl::PointCloud<pcl::PointXYZI>::Ptr combined_cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  cartographer_ros_msgs::SubmapCloudQuery query;
+  query.request.trajectory_id = 0;
+  query.request.submap_index = -1;
+  query.request.min_probability = 0.1;
+  query.request.high_resolution = true;
+  client_.call(query);
 
-  for (int submap_index : submap_indices_)
-  {
-    cartographer_ros_msgs::SubmapCloudQuery query;
-    query.request.trajectory_id = 0;
-    query.request.submap_index = submap_index;
-    query.request.min_probability = 0.1;
-    query.request.high_resolution = true;
-    client_.call(query);
+  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
+  pcl::fromROSMsg(query.response.cloud, *cloud);
 
-    pcl::PointCloud<pcl::PointXYZI>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZI>);
-    pcl::fromROSMsg(query.response.cloud, *cloud);
+  sensor_msgs::PointCloud2 cloud_msg;
+  pcl::toROSMsg(*cloud, cloud_msg);
+  cloud_msg.header.stamp = ros::Time::now();
+  cloud_msg.header.frame_id = "map";
 
-    *combined_cloud += *cloud;
-  }
-
-  sensor_msgs::PointCloud2 combined_cloud_msg;
-  pcl::toROSMsg(*combined_cloud, combined_cloud_msg);
-  combined_cloud_msg.header.stamp = ros::Time::now();
-  combined_cloud_msg.header.frame_id = "map";
-
-  cloud_pub_.publish(combined_cloud_msg);
-}
-
-void MappingCloudPublisher::submapListCallback(const cartographer_ros_msgs::SubmapList::ConstPtr submap_list)
-{
-  submap_indices_.clear();
-  for (const auto& entry : submap_list->submap)
-  {
-    if (entry.trajectory_id == 0)
-    {
-      submap_indices_.push_back(entry.submap_index);
-    }
-  }
+  cloud_pub_.publish(cloud_msg);
 }
 
 int main(int argc, char** argv)
