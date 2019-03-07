3702ad4da74dfb30021113bf9780f036472e7e5f
zhengqunkoo <zhengqun.koo@gmail.com>
2019-03-07 14:16:56 +0800

    Remove git symbols

--- firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/message_template/message_template.h
+++ firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/message_template/message_template.h
@@ -1,27 +1,27 @@
--// Frames related
--#define NUM_STAMPS_PER_NODE 2 // Number of timestamps stored that belong to each node
--#define DATA_LEN NUM_STAMPS_PER_NODE*(N-1)+1 // Length (bytes) of data in standard message
--#define MSG_LEN 13+DATA_LEN // Length (bytes) of the standard message
- 
--/** Message template
-- *
-- * Note, at most 127 bytes long.
-- *
-- * header: usually fixed.
-- * id: id of node that sent this message.
-- * isFirst: whether this message is first in the cycle.
-- * data: time.
-- *  time: uint32, DATA_LEN timestamps.
-- *    |0|0|1|1|...|N-1|N-1|
-- *    where NODE_ID is i
-- *      i elems: actual tx time, estimated transmission time
-- *      other elems: rx time, rx time
-- * crc: needs to be the final two bytes.
-- */
--typedef struct {
--  uint8 header[10];
--  uint8 id;
--  bool isFirst;
--  uint8 data[DATA_LEN];
--  uint8 crc[2];
--} msg_template
+// Frames related
+#define NUM_STAMPS_PER_NODE 2 // Number of timestamps stored that belong to each node
+#define DATA_LEN NUM_STAMPS_PER_NODE*(N-1)+1 // Length (bytes) of data in standard message
+#define MSG_LEN 13+DATA_LEN // Length (bytes) of the standard message
+
+/** Message template
+ *
+ * Note, at most 127 bytes long.
+ *
+ * header: usually fixed.
+ * id: id of node that sent this message.
+ * isFirst: whether this message is first in the cycle.
+ * data: time.
+ *  time: uint32, DATA_LEN timestamps.
+ *    |0|0|1|1|...|N-1|N-1|
+ *    where NODE_ID is i
+ *      i elems: actual tx time, estimated transmission time
+ *      other elems: rx time, rx time
+ * crc: needs to be the final two bytes.
+ */
+typedef struct {
+  uint8 header[10];
+  uint8 id;
+  bool isFirst;
+  uint8 data[DATA_LEN];
+  uint8 crc[2];
+} msg_template
