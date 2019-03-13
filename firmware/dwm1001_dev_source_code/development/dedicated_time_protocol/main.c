a67f6c70040b3a617048b9eab5371622fd3b7fe2
zhengqunkoo <zhengqun.koo@gmail.com>
2019-03-13 21:41:56 +0800

    Remove unused protocol

--- firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/main.c
+++ firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/main.c
@@ -276,30 +276,6 @@ void runTask (void * pvParameter)
   }
 }
 
-void enterNetwork(int id) {
-  nodeListen();
-  nodeSleep();
-  nodeLoop(id);
-}
-
-void nodeLoop(int id) {
-  while(true) {
-    nodeWakeUp();
-    nodeProtocol(id);
-    nodeSleep(id);
-  }
-}
-
-void nodeListen() {
-
-  /* Clear reception timeout to start next ranging process. */
-  dwt_setrxtimeout(0);
-
-  /* Activate reception immediately. */
-  dwt_rxenable(DWT_START_RX_IMMEDIATE);
-
-}
-
 /**
  * @brief Stores rx data in the correct buffer.
  * If rx request, store rx id, for subsequent transmission.
@@ -412,34 +388,6 @@ msg_template getTimestamps(uint8 isFirst) {
   In other words, we need to have a "fast updating" of the data structure whenever RX interrupt occurs, so the
   node does not spend too much time computating, and can move on to be ready for the next RX interrupt.
 */
-/**
- * @brief Protocol implementation
- *
- * @param[id] ID of node executing this protocol
- * If ID is 1, then broadcast immediately
- * Else, receive previous ID's transmission, then broadcast
- */
-void nodeProtocol(int id) {
-
-  // TODO reimplement with RX and TX that update global vars on callback
-
-  for (int i = 1; i < id; i++) {
-    setTimestamps();
-  }
-
-  getMsgEmpty();
-
-  for (int i = 1; i < N; i++) {
-    setTimestamps();
-  }
-
-  getTimestamps();
-
-  for (int i = id; i < N; i++) {
-    setTimestamps();
-  }
-}
-
 /* Protocol functions */
 
 /**
