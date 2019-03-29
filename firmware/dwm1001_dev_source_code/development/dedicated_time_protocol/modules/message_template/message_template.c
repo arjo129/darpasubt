95f3c0fc3d48491598cc7b46fb7c449373912823
zhengqunkoo <zhengqun.koo@gmail.com>
2019-03-29 13:46:08 +0800

    Abstract out NUM_STAMPS_PER_NODE

--- firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/modules/message_template/message_template.c
+++ firmware/dwm1001_dev_source_code/development/dedicated_time_protocol/modules/message_template/message_template.c
@@ -137,16 +137,16 @@ void updateTable(uint64 table[NUM_STAMPS_PER_CYCLE][N], msg_template msg, uint64
 
     if (thisId < msg.id)
     {
-      memcpy(&table[IDX_TS_2][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4), sizeof(uint64));
-      memcpy(&table[IDX_TS_3][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4) + 4, sizeof(uint64));
-      memcpy(&table[IDX_TS_6][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4) + 8, sizeof(uint64));
+      memcpy(&table[IDX_TS_2][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP), sizeof(uint64));
+      memcpy(&table[IDX_TS_3][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP) + NUM_BYTES_PER_STAMP, sizeof(uint64));
+      memcpy(&table[IDX_TS_6][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP) + 2*NUM_BYTES_PER_STAMP, sizeof(uint64));
     }
     else if (thisId > msg.id)
     {
       table[IDX_TS_6][msg.id] = ts; // Store the reception timestamp
-      memcpy(&table[IDX_TS_1][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4), sizeof(uint64));
-      memcpy(&table[IDX_TS_4][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4) + 4, sizeof(uint64));
-      memcpy(&table[IDX_TS_5][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * 4) + 8, sizeof(uint64));
+      memcpy(&table[IDX_TS_1][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP), sizeof(uint64));
+      memcpy(&table[IDX_TS_4][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP) + NUM_BYTES_PER_STAMP, sizeof(uint64));
+      memcpy(&table[IDX_TS_5][msg.id], msg.data + (thisId * NUM_STAMPS_PER_NODE * NUM_BYTES_PER_STAMP) + 2*NUM_BYTES_PER_STAMP, sizeof(uint64));
     }
     else
     {
