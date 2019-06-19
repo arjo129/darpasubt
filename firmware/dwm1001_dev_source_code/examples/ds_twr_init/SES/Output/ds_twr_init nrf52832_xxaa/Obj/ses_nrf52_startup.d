a2ea5ae5038c15ff48dfc74f7fdb4d422782db88
lekoook <kokteng1313@gmail.com>
2019-06-19 18:15:36 +0800

    Cleaning and rename project files

--- firmware/dwm1001_dev_source_code/examples/ds_twr_init/SES/Output/ds_twr_init nrf52832_xxaa/Obj/ses_nrf52_startup.d
+++
@@ -1,2 +0,0 @@
-Output/ds_twr_init\ nrf52832_xxaa/Obj/ses_nrf52_startup.o: \
- C:\Users\hashi\Desktop\temp\darpasubt\firmware\dwm1001_dev_source_code\nRF5_SDK_14.2.0\components\toolchain\ses\ses_nrf52_startup.s
--- firmware/dwm1001_dev_source_code/examples/ds_twr_init/SES/Output/ds_twr_init nrf52832_xxaa/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/ds_twr_init\ nrf52832_xxaa/Obj/thumb_crt0.o: \
- C:\Users\hashi\Desktop\temp\darpasubt\firmware\dwm1001_dev_source_code\examples\ds_twr_init\SES\RTE\thumb_crt0.s
